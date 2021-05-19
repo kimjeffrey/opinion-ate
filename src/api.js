af4f0c320e17acb9f0513c87786978175673dfe4
Jeffrey Kim <jeffreyk93@gmail.com>
2021-05-19 15:56:50 -0400

    Add api client with axios

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'https://outside-in-dev-api.herokuapp.com/dR5mZOOUsHllIbRnyjGsYC0u9IkEkeUx',
+})
+
+const api = {
+  loadRestaurants() {
+    return client.get('/restaurants').then(res => res.data);
+  },
+};
+
+export default api;
